ec81beff83091e3d2f5d4f8846f5e4380ff20529
U-BSD\298970 <298970@SHS-10DD3C429.beaverton.k12.or.us>
2017-09-11 10:49:09 -0700

    Finished mots of it

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ int main(){
   bool test= true;
   while(test){
     //Seed the pseudorandom generator
-    srand(time(NULL));
+    srand((unsigned)time(NULL));
     //Pick a random int for the user to guess between 1 and 100
     int randInt= (rand()%101);
     //Check if the user has completed the game yet
@@ -18,9 +19,9 @@ int main(){
     while(playing){
 	guessCount++;
     	//Get an input from the user
-    	cout << "Guess a number between 0 and 100"<<endl;
-    	int guess;
-    	cin << guess;
+	int guess;
+    	cout << "Guess a number between 0 and 100: ";
+    	cin >>  guess;
 	//Check to make sure the guess is vaild, make sure the loop continues if it isn't
 	if(guess>100 || guess<0){
 		cout << "Your input was invalid";
@@ -28,8 +29,28 @@ int main(){
 	}else{
 		//If they guess correctly, then it should trigger the win condition
 		if(guess == randInt){
-			cout <<
+			cout << "Congratulations! You have won the game.";
+			cout << "Would you like to play again? (y/n)";
+			char c[1];
+			cin >> c;
+			//Check if they want to play again and respond appropriately
+			if(tolower(c[0]) == 'y'){
+				test = true;
+			}
+			else{
+				test = false;
+			}
 		}
+		//If the guess is too high tell the user that
+		else if(guess>randInt){
+			cout << "Your guess was too high" << endl;
+		}
+		//Otherwise the user's guess was too low
+		else{
+			cout << "Your guess was too low" << endl;
+		}
+
+
 	}
     }
 
